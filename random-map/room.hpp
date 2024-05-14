c201b77b6c6c77e35b73b12dec26e851398fd804
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-05-14 11:57:28 +0900

    Update room.hpp

--- random-map/room.hpp
+++ random-map/room.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <vector>
+#include <utility>
 
 class room {
   private:
     vector<vector<bool>> _room; // trueが通路、falseが壁
-    const int _width, _height;
+    const int _width, _height, _block;
   public:
-    room(const int width, const int height, const int block);
+    room(const int width, const int height, const int block, const& vector<std::pair<int, int>> connected);
 }
