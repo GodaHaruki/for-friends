087a6fb79d5d2e193b340bec42cb5be45d463f8a
Okay-Love <haru.11.saki.11@gmail.com>
2024-05-14 18:39:01 +0900

    fix error

--- random-map/room.cpp
+++ random-map/room.cpp
@@ -5,7 +5,7 @@
 
 using pos = std::pair<int, int>;
 
-bool is_surrounded_by_path(pos current, const& vector<vector<bool>> map){
+bool is_surrounded_by_path(pos const current, const std::vector<std::vector<bool>>& map){
   if(current.first -1 < 0 && map[current.first -1][current.second]){
     return false;
   } else if(current.first +1 < map.size() && map[current.first +1][current.second]){
@@ -19,11 +19,11 @@ bool is_surrounded_by_path(pos current, const& vector<vector<bool>> map){
   return true;
 }
 
-int distance(pos pos1, pos pos2){
+int distance(pos const pos1, pos const pos2){
   return abs(pos1.first - pos2.first) + abs(pos1.second - pos2.second);
 }
 
-void dig(const pos current, const int can_dig, vector<vector<bool>>& map, const& vector<pos> connected){
+void dig(pos const current, int const can_dig, const std::vector<std::vector<bool>>& map, const std::vector<pos>& connected){
   if(can_dig == 0){
     return;
   }
@@ -35,7 +35,7 @@ void dig(const pos current, const int can_dig, vector<vector<bool>>& map, const&
   return dig(new_pos, can_dig -1, map, connected);
 }
 
-room::room(const int width, const int height, const int block, const& vector<pos> connected)
-: _width(width), _height(height), _block(block),
+room::room(const int width, const int height, const int block, const std::vector<pos>& connected)
+: _width(width), _height(height), _block(block)
 {
 }
--- random-map/room.hpp
+++ random-map/room.hpp
@@ -1,11 +1,10 @@
-#pragma once
 #include <vector>
 #include <utility>
 
 class room {
   private:
-    vector<vector<bool>> _room; // trueが通路、falseが壁
+    std::vector<std::vector<bool>> _map; // trueが通路、falseが壁
     const int _width, _height, _block;
   public:
-    room(const int width, const int height, const int block, const& vector<std::pair<int, int>> connected);
-}
+    room(const int width, const int height, const int block, const std::vector<std::pair<int, int>>& connected);
+};
