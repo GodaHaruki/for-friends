8681b01628988ada1a1957a9243f6a4711f44bed
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-05-14 15:22:06 +0900

    Update room.cpp

--- random-map/room.cpp
+++ random-map/room.cpp
@@ -5,61 +5,37 @@
 
 using pos = std::pair<int, int>;
 
-int dig_inner(pos current, int can_dig, vector<vector<bool>>& map, vector<int>& min_distances, const vector<pos>& connected){
-  if(can_dig - accumulate(min_distances.begin(), min_distances.end(), 0) <= 0){
-    return can_dig;
+bool is_surrounded_by_path(pos current, const& vector<vector<bool>> map){
+  if(current.first -1 < 0 && map[current.first -1][current.second]){
+    return false;
+  } else if(current.first +1 < map.size() && map[current.first +1][current.second]){
+    return false;
+  } else if(current.second -1 < 0 && map[current.first][current.second -1]){
+    return false;
+  } else if(current.second +1 < map.size() && map[current.first][current.second +1]){
+    return false;
   }
 
-  // todo
-
-  return dig_inner(current, can_dig, map, min_distance, connected);
+  return true;
 }
 
-void dig(pos current, int can_dig, vector<vector<bool>>& map, vector<int>& min_distances, const vector<pos>& connected){
-  can_dig = dig_inner(current, can_dig, map, min_distances, connected);
+int distance(pos pos1, pos pos2){
+  return abs(pos1.first - pos2.first) + abs(pos1.second - pos2.second);
+}
 
-  
-  if(can_dig > 0){
+void dig(const pos current, const int can_dig, vector<vector<bool>>& map, const& vector<pos> connected){
+  if(can_dig == 0){
+    return;
   }
+  
+  std::random_device rd;
+  std::mt19937 mt(rd());  
+
+  pos new_pos = {0, 0};
+  return dig(new_pos, can_dig -1, map, connected);
 }
 
 room::room(const int width, const int height, const int block, const& vector<pos> connected)
 : _width(width), _height(height), _block(block),
 {
-  _m = vector<vector<bool>>(height, vector<bool>(width, false));
-  int connection_count = connected.size();
-  
-  std::random_device rd;
-  std::mt19937 mt(rd());
-
-  pos current = {0, 0};
-  vector<int> min_distances(0, 0);
-  int digged = 0;
-  if(connected.size() > 0){
-    current = connected[0];
-    min_distances.resize(connection_count);
-  }
-
-  auto sum_distances = [&](){
-    int res = 0;
-    for(auto d : min_distances){
-      res += d;
-    }
-
-    return res;
-  };
-
-  auto distance = [](pos pos1, pos pos2){
-    return abs(pos1.first - pos2.first) + abs(pos1.second - pos2.second);
-  };
-  
-  while(block - sum_distances() > digged){
-    _map[current.first][current.second] = true;
-    for(int i = 0; i < connection_count; i++){
-      pos c = connected[i];
-      if(min_distances[i] > distance(c, current)){
-        min_distance[i] = distance(c, current);
-      }
-    }
-  }
 }
