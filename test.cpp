a9e55aad5555260b9f26552501abd6195ce93243
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-05-15 11:18:54 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,10 +7,10 @@ int main(){
   
   connected.push_back({5, 0});
   connected.push_back({0, 5});
-  // connected.push_back({19, 19});
+  connected.push_back({19, 19});
   // connectedを2つ以外にするとセグフォ起きる
 
-  room r(20, 20, 10, connected);
+  room r(20, 20, 100, connected);
 
   r.show();
 }
