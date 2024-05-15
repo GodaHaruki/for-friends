ea970c3f1669e4598b0561e41d673d0da49bee5c
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-05-15 12:34:36 +0900

    テストケースを追加

--- test.cpp
+++ test.cpp
@@ -7,8 +7,10 @@ int main(){
   
   connected.push_back({5, 0});
   connected.push_back({0, 5});
-  connected.push_back({19, 19});
-  // connectedを2つ以外にするとセグフォ起きる
+
+  room r1(30, 30, 450, connected);
+
+  r1.show();
 
   room r2(23, 37, 63, connected);
 
@@ -16,7 +18,12 @@ int main(){
 
   connected.push_back({20, 20});
   connected.push_back({18, 13});
+  connected.push_back({27, 27});
   room r3(34, 28, 43, connected);
 
   r3.show();
+
+  room r4(200, 200, 20000, connected);
+
+  r4.show();
 }
